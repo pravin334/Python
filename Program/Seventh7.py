ba5f681e38ef651a0d7c2c74032f000a095890a6
Pravin <pravin.jagatap@thinkitive.com>
2024-01-23 18:48:36 +0530

    Adding new File

---
+++ Program/Seventh7.py
@@ -0,0 +1,15 @@
+
+input = float(input("Enter a numeric value: "))
+
+if input < 35:
+    print("Fail")
+elif 35 <= input < 60:
+    print("Average")
+elif 60 <= input < 75:
+    print("Good")
+elif 75 <= input < 90:
+    print("Distinction")
+elif input>100:
+    print("Please Enter correct value")
+else:
+    print("|| सीता राम हनुमान ||")
---
+++ Program/Third3.py
@@ -0,0 +1,8 @@
+i= int(input("Enter number - "))
+
+j = "||जय श्री राम||"
+
+for k in range(i):
+    print(j)
+
+
